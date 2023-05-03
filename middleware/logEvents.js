2c1cf7a83a15b941ea99ad23aba27940c69338d8
jakehase <jacobhasenkamp@gmail.com>
2023-05-02 19:43:13 -0500

    Update logEvents.js

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,25 +1,31 @@
-// const { format } = require('date-fns');
-// const { v4: uuid } = require('uuid');
-
 const fs = require('fs');
-const fsPromises = require('fs').promises;
+const fsPromises = fs.promises;
 const path = require('path');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
 
+// Function to create logs directory if it doesn't exist
+const createLogsDir = async () => {
+    const logsDirPath = path.join(__dirname, '..', 'logs');
+    if (!fs.existsSync(logsDirPath)) {
+        await fsPromises.mkdir(logsDirPath);
+    }
+}
+
+// Function to log events
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
     try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
-        }
-
+        await createLogsDir();
         await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
     }
 }
 
+// Middleware function to log requests
 const logger = (req, res, next) => {
     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
     console.log(`${req.method} ${req.path}`);
